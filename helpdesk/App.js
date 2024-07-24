1bff5fe3c33f56054eaae9c70b505a5068f4ba1c
vallent14 <126632291+vallent14@users.noreply.github.com>
2024-07-24 18:17:07 +0700

    Help desk

---
+++ helpdesk/App.js
@@ -0,0 +1,12 @@
+import { NavigationContainer } from "@react-navigation/native";
+import Router from "./Router/index.js";
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Router />
+    </NavigationContainer>
+  )
+}
+
+export default App;
